bc199a9dfc884b12895f0da03edcdda9832753cd
xnguyen9a10 <xnguyen9a10@gmail.com>
2020-06-30 15:15:05 +0700

    refactoring

--- TaskOutside.js
+++ TaskOutside.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
  
 // Task component - represents a single todo item
-export default class Task2 extends Component {
+export default class TaskOutside extends Component {
   render() {
     return (
       <li>{this.props.task.text}xx</li>
